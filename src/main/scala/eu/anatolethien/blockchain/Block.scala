e808e69b2a6a29a60f2e5bca1ccd27a05cd26be9
anatolethien <mail@anatolethien.eu>
2024-03-24 18:47:05 +0100

    private values, scaladoc

--- src/main/scala/eu/anatolethien/blockchain/Block.scala
+++ src/main/scala/eu/anatolethien/blockchain/Block.scala
@@ -2,11 +2,18 @@ package eu.anatolethien.blockchain
 
 import java.security.MessageDigest
 
+/**
+  * Implementation of a block in the blockchain. A block is a hash of
+  * concatenation of the previous block's hash and the transaction data
+  * @param previousBlockHash the hash of the previous block in the blockchain
+  * @param transaction the transaction data to be stored in this block
+  */
 class Block(val previousBlockHash: String, val transaction: Transaction) {
-    // Concatenation of previousBlockHash and transaction data
-    val data = List(transaction, previousBlockHash).mkString("\n")
-    // New block hash
-    val md = MessageDigest.getInstance("SHA-256")
-    val bytes = md.digest(data.getBytes("UTF-8"))
-    val hash = bytes.map(byte => String.format("%02x", byte.asInstanceOf[Object])).mkString
+
+  private val data = List[Transaction | String](transaction, previousBlockHash).mkString("\n")
+
+  private val md = MessageDigest.getInstance("SHA-256")
+  private val bytes = md.digest(data.getBytes("UTF-8"))
+  val hash = bytes.map(byte => String.format("%02x", byte.asInstanceOf[Object])).mkString
+
 }
