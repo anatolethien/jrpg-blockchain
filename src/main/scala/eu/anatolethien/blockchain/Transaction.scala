92fb4a014eb7bba8fa7fb3b09226b1f2de6bd87e
anatolethien <mail@anatolethien.eu>
2024-03-24 15:15:43 +0100

    toCsv method implementation, scaladoc

--- src/main/scala/eu/anatolethien/blockchain/Transaction.scala
+++ src/main/scala/eu/anatolethien/blockchain/Transaction.scala
@@ -1,7 +1,20 @@
 package eu.anatolethien.blockchain
 
+/** Implementation of an economic transaction between two parties.
+ *  @param emitter the emitter's identifier
+ *  @param receiver the receiver's identifier
+ *  @param amount the amount of the transaction.
+ */
 class Transaction(val emitter: Int, val receiver: Int, val amount: Int) {
-    override def toString: String = {
+
+    /** String representation of the transaction as a CSV
+     *  @return a string representation of the transaction as a CSV.
+     *  @example `Transaction(2, 3, 10).toCsv() = "2,3,10"`
+     */
+    def toCsv: String = {
         List(emitter, receiver, amount).mkString(",")
     }
+
+    override def toString: String = toCsv
+
 }
